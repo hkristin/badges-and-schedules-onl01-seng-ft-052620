0442a63ca6c4447e0d565a27d907ff1b892b82e1
Kristin Howard <hkristin@andrew.cmu.edu>
2020-05-28 19:25:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(attendees)
-  attendees.map_with_index do |attendee, index|
+  attendees.map.with_index do |attendee, index|
     "Hello, #{attendee}! You'll be assigned to room #{index}!"
   end
 end
