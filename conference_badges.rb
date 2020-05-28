bae175c44ee208710f52bca9fcf685b0aa28fc2e
Kristin Howard <hkristin@andrew.cmu.edu>
2020-05-28 18:36:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  return_array = []
+  attendees.each do |attendee|
+    return_array << badge_maker(attendee)
+end
+  return_array
+end
+
+def assign_rooms(speakers)
+  room_assignments
+  speakers.each_with_index do |speaker, index|
+    
+  end
+end
+
+def printer
+  
+end
